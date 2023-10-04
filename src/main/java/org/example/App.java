c24876c540db3d4fa9674490401a80f98ee546c8
vitaliiSh <vitalii.shkar@gmail.com>
2023-10-04 20:15:45 +0300

    Add a movie with two actors

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * This is a simple hibernate application that demonstrates
  * how to work with a Many-To-Many relationship
@@ -22,6 +26,20 @@ public class App {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
+            // Add a movie with two actors to the database
+            Movie movie = new Movie("Pulp fiction", 1994);
+            Actor actor1 = new Actor("Harvey Keitel", 81);
+            Actor actor2 = new Actor("Samuel L. Jackson", 72);
+
+            // This is similar to Arrays.asList().
+            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+
+            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+
+            session.save(movie);
+            session.save(actor1);
+            session.save(actor2);
 
             session.getTransaction().commit();
         }
